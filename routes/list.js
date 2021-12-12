738a8bde2caf982bdc558d62c2b418f3866e712d
Mario Espada <mario.espada77@gmail.com>
2021-12-12 19:11:17 +0100

    ADD get all list for a user and get list in detail

--- routes/list.js
+++ routes/list.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const List = require("../models/List");
 const Movie = require("../models/Movie");
-const { checkIfLoggedIn } = require("../middlewares");
+const { checkIfLoggedIn, ObjectIdIsValid } = require("../middlewares");
 const router = express.Router();
 
 router.post("/create", checkIfLoggedIn, async (req, res, next) => {
@@ -35,7 +35,7 @@ router.get("/all", checkIfLoggedIn, async (req, res, next) => {
           * Return all lists of the users  
     */
     try {
-        const lists = await List.find({});
+        const lists = await List.find({}).populate("user").select("-hashedPassword");;
         if(lists) {
             res.status(200).json({  message: "Lists retrieved successfully", lists });
         }else{
@@ -45,6 +45,58 @@ router.get("/all", checkIfLoggedIn, async (req, res, next) => {
         console.log(error);
     }
 
+
+});
+
+router.get("/:userId/all", checkIfLoggedIn, async (req, res, next) => {
+    /*
+          * Get all lists of a user
+          * Requires username id to get all lists of a user
+          * Return all lists of the user 
+    */
+    const { userId } = req.params;
+    const username = req.session.currentUser;
+    try { 
+        if(ObjectIdIsValid(userId)) {
+            const lists = await List.find({ user: username._id }).populate("user").select("-hashedPassword");;
+            if(lists) {
+                res.status(200).json({  message: "Lists retrieved successfully", lists });
+            }else{
+                res.status(500).json({ message: "Error retrieving Lists "});
+            }
+        }else{
+            res.status(500).json({ message: "Invalid user ID"});
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+router.get("/:listId/detail", checkIfLoggedIn, async (req, res, next) => {
+    /*
+          * Get list in detail
+          * Requires list id to get all lists of a user
+          * Return a single list
+    */
+    const { listId } = req.params;
+    const username = req.session.currentUser;
+    try { 
+        if(ObjectIdIsValid(listId)) {
+            const lists = await List.findOne({ _id: listId }).populate("user").select("-hashedPassword");
+            if(lists) {
+                res.status(200).json({  message: "Lists retrieved successfully", lists });
+            }else{
+                res.status(500).json({ message: "Error retrieving Lists "});
+            }
+        }else{
+            res.status(500).json({ message: "Invalid list ID"});
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
 
 });
 
